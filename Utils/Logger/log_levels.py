204d64e93de3f275ee873ba20fc097ad14d219fc
ViNN280801 <vladislav_semykin01@mail.ru>
2025-01-13 09:43:53 +0300

    Added doxy description for the enums
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

--- Utils/Logger/log_levels.py
+++ Utils/Logger/log_levels.py
@@ -3,6 +3,20 @@ from enum import Enum
 
 
 class LogLevel(Enum):
+    """
+    Enumeration for log levels.
+
+    This enumeration maps Python's logging levels to a more structured format.
+    It can be used to specify logging verbosity in applications.
+
+    Attributes:
+        INFO: Informational messages.
+        DEBUG: Debugging messages.
+        WARNING: Warning messages indicating potential issues.
+        ERROR: Error messages for failures that require attention.
+        CRITICAL: Critical error messages for severe failures.
+    """
+
     INFO = logging.INFO
     DEBUG = logging.DEBUG
     WARNING = logging.WARNING
