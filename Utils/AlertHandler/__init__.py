0ad42da00dcc2e3cef5e4179d9573475c6e044fa
ViNN280801 <vladislav_semykin01@mail.ru>
2025-01-14 16:16:56 +0300

    There need to implement functional tests
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

--- Utils/AlertHandler/__init__.py
+++
@@ -1,4 +0,0 @@
-from .alert_destination import AlertDestination
-from .alert_handler import AlertHandler
-
-__all__ = ["AlertDestination", "AlertHandler"]
--- Utils/ExceptionHandler/__init__.py
+++
@@ -1,3 +0,0 @@
-from .exception_handler import ExceptionHandler
-
-__all__ = ["ExceptionHandler"]
